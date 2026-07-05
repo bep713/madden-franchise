f9d473c57e21656cea8911cd24f51ac40742d392
WiiExpertise <gpstore31@gmail.com>
2026-07-04 23:39:27 -0500

    Add very light basic tests for CFB 27 files

--- tests/util/filePathUtil.js
+++ tests/util/filePathUtil.js
@@ -5,19 +5,22 @@ export default {
         m24: 'tests/data/CAREER-24COMPRESS',
         m25: 'tests/data/CAREER-25COMPRESS',
         m26: 'tests/data/CAREER-26COMPRESS',
-        m26Ftc: 'tests/data/M26_FTC_COMPRESS.FTC'
+        m26Ftc: 'tests/data/M26_FTC_COMPRESS.FTC',
+        c27: 'tests/data/DYNASTY-27COMPRESS'
     },
     uncompressed: {
         m21: 'tests/data/21UNCOMPRESS.frt',
         m24: 'tests/data/24UNCOMPRESS.frt',
         m25: 'tests/data/25UNCOMPRESS.frt',
-        m26: 'tests/data/26UNCOMPRESS.frt'
+        m26: 'tests/data/26UNCOMPRESS.frt',
+        c27: 'tests/data/C27UNCOMPRESS.frt'
     },
     saveTest: {
         m21: 'tests/data/CAREER-TESTSAVE-21',
         m23: 'tests/data/CAREER-TESTSAVE-23',
         m24: 'tests/data/CAREER-TESTSAVE-24',
         m25: 'tests/data/CAREER-TESTSAVE-25',
-        m26: 'tests/data/CAREER-TESTSAVE-26'
+        m26: 'tests/data/CAREER-TESTSAVE-26',
+        c27: 'tests/data/DYNASTY-TESTSAVE-27'
     }
 };
